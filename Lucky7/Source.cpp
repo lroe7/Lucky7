c2b21e45156ad1608f4d5e5d21b3f65a3633dbb1
laure <laure@DESKTOP-HRJTM3L>
2020-01-14 09:29:15 -0700

    Add project files.

---
+++ Lucky7/Source.cpp
@@ -0,0 +1,16 @@
+//Lauren Roe
+//This is my own work
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	// Write a program that reads in 5 integers and prints the largest and the smallest of the group.
+	int a, b, c, d, e;
+	cout << "Enter 5 integers: " << endl;
+	cin >> a >> b >> c >> d >> e;
+	cout << "The numbers are " <<a <<" "<<b<<" "<<c<<" "<<d<<" "<<e<< endl;
+	return 0;
+}
