1183bcd3b349d7cad6590fe13a0bd869c6977bdd
laure <laure@DESKTOP-HRJTM3L>
2020-01-14 09:45:16 -0700

    added find largest logic to part 1

--- Lucky7/Source.cpp
+++ Lucky7/Source.cpp
@@ -11,6 +11,14 @@ int main()
 	int a, b, c, d, e;
 	cout << "Enter 5 integers: " << endl;
 	cin >> a >> b >> c >> d >> e;
-	cout << "The numbers are " <<a <<" "<<b<<" "<<c<<" "<<d<<" "<<e<< endl;
+	//cout << "The numbers are " <<a <<" "<<b<<" "<<c<<" "<<d<<" "<<e<< endl;
+	int largest = a;
+	if (b > largest) largest = b;
+	if (c > largest) largest = c;
+	if (d > largest) largest = d;
+	if (e > largest) largest = e;
+	cout << largest << endl;
+	int smallest = a;
+
 	return 0;
 }
