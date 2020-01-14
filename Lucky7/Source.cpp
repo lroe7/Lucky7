6d2630d252c09e630bec20369e3ca93aff7b5cbf
laure <laure@DESKTOP-HRJTM3L>
2020-01-14 09:56:54 -0700

    Finished part 1

--- Lucky7/Source.cpp
+++ Lucky7/Source.cpp
@@ -11,14 +11,21 @@ int main()
 	int a, b, c, d, e;
 	cout << "Enter 5 integers: " << endl;
 	cin >> a >> b >> c >> d >> e;
-	//cout << "The numbers are " <<a <<" "<<b<<" "<<c<<" "<<d<<" "<<e<< endl;
+	cout << "The numbers are " <<a <<" "<<b<<" "<<c<<" "<<d<<" "<<e<< endl;
 	int largest = a;
 	if (b > largest) largest = b;
 	if (c > largest) largest = c;
 	if (d > largest) largest = d;
 	if (e > largest) largest = e;
-	cout << largest << endl;
+	cout << "The largest number is " << largest << endl;
+	
 	int smallest = a;
+	if (b < smallest) smallest = b;
+	if (c < smallest) smallest = c;
+	if (d < smallest) smallest = d;
+	if (e < smallest) smallest = e;
+	cout << "The smallest number is " << smallest << endl;
 
+	
 	return 0;
 }
