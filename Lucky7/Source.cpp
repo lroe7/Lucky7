ead3897d57e58ab476b6c6e39abbb5788c3cb474
laure <laure@DESKTOP-HRJTM3L>
2020-01-21 10:22:13 -0700

    Finished part 7

--- Lucky7/Source.cpp
+++ Lucky7/Source.cpp
@@ -2,6 +2,7 @@
 //This is my own work
 
 #include <iostream>
+#include <fstream>
 #include <string>
 
 using namespace std;
@@ -27,8 +28,7 @@ int main()
 	if (e < smallest) smallest = e;
 	cout << "The smallest number is " << smallest << endl;*/
 
-	//2. Write a program that calculates and prints the sum of the first 
-	//50 positive integers that are multiples of 7.
+	//2. Write a program that calculates and prints the sum of the first 50 positive integers that are multiples of 7.
 	/*int sum = 0; 
 	for (int i = 1; i <= 50; i++)
 	{
@@ -65,6 +65,7 @@ int main()
 	{
 		cout << str << " is a palindrome." << endl;
 	}*/
+	
 	//5. Write a program that reads in a positive integer and determines whether it is a prime number.
 	/*cout << "Please enter a number: " << endl;
 	int num;
@@ -83,20 +84,63 @@ int main()
 	{
 		cout << num << " is a prime number" << endl;
 	}*/
-	//6. Write a program that creates an array of integers using an initialization list and 
-	//calculates and displays the average of the values.
-	double sum = 0.0;
+	
+	//6. Write a program that creates an array of integers using an initialization list and calculates and displays the average of the values.
+	/*double sum = 0.0;
 	int count = 0;
 	int a, b, c, d, e;
 	cout << "Please enter 5 integers: " << endl;
 	cin >> a >> b >> c >> d >> e;
 	int arr[] = { a, b, c, d, e };
-	for (int i = 0; i < sizeof(arr)/4; i++)
+	for (int i = 0; i < sizeof(arr)/sizeof(int); i++)
 	{
 		sum += arr[i];
 		count++;
 	}
 	double average = sum / count;
-	cout <<"The average of ["<<a<<", "<<b<<", "<<c<<", "<<d<<", "<<e<<"] is "<<average<<endl;
+	cout <<"The average of ["<<a<<", "<<b<<", "<<c<<", "<<d<<", "<<e<<"] is "<<average<<endl;*/
+	
+	//7. Write a program that gets a positive integer n from the user and creates an array of n. 
+	//The program should read strings from a file, fill the array with the first n strings, and print the string with the most characters. 
+	//You may assume that number of strings in the file is greater than n. 
+	int n;
+	string* wordList;
+	cout << "Please enter an integer for the size of the array: " << endl;
+	cin >> n;
+	
+	wordList = new string[n];
+
+	//create an object to read from file
+	ifstream fin("input.in");
+
+	//make sure fin is initialized to the file
+	if (!fin)
+	{
+		cerr << "file not found" << endl;
+		exit(1);
+	}
+	int i = 0; 
+	while (i < n)
+	{
+		fin >> wordList[i];
+		i++;
+	}
+	for (int j = 0; j < n; j++)
+	{
+		cout << wordList[j] << " ";
+	}
+	cout << '\n';
+	string largest = wordList[0];
+	for (int k = 1; k < n; k++)
+	{
+		if (wordList[k].size() > largest.size())
+		{
+			largest = wordList[k];
+		}
+	}
+	cout << "The largest word is " << largest << endl;
+
+	delete[] wordList;
+
 	return 0;
 }
